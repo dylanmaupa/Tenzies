61712a2050f6a16185ab35d3040fb48669c4dee0
dylanmaupa <114060288+dylanmaupa@users.noreply.github.com>
2024-06-08 15:45:14 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [tenzies, setTenzies] = useState(false);
+  const [dices, setDices] = useState(allNewDice);
+  const [roll, setRoll] = useState(0);
+  const [start, setStart] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  if (seconds > 59) {
+    setSeconds(0);
+  }
+  if (minutes > 59) {
+    setMinutes(0);
+    setHours(hour => hour + 1);
+  }
+  if (hours > 23) {
+    setHours(0);
+  }
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      if (!start) {
+        return;
+      }
+      if (tenzies) {
+        return;
+      }
+      setSeconds(second => second + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [start, !tenzies]);
+
+  useEffect(() => {
+    let value = dices[0].value;
+    let isHeld = dices.every(dice => dice.isHeld);
+    let isSameValue = dices.every(dice => dice.value === value);
+    if (isSameValue && isHeld) {
+      setTenzies(true);
+    }
+    else {
+      setTenzies(false);
+    }
+  }, [dices]);
+
+  function allNewDice() {
+    const newArray = [];
+    for (let i = 0; i < 10; i++) {
+      newArray.push({ id: nanoid(), value: (Math.floor(Math.random() * 6) + 1), isHeld: false });
+    }
+    return newArray
+  }
+
+  function holdDice(id){
+    setStart(true)
+    if (tenzies){
+      return
+    }
+    setDices(dices => dices.map(dice => {
+      return dice.id === id ? {...dice, isHeld: !dice.isHeld} : dice
+    }))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <main>
+      <h1 className="heading">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+    </main>
+  );
+};
+ export default App;
